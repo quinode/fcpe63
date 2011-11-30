53a01a2a11e9584ada3ab5d6a77638a330a7020d
Dominique Guardiola <contact@quinode.fr>
2011-11-30 15:33:15 +0100

    mailing_update : responsables - fix

--- fcpe/management/commands/update_lists.py
+++ fcpe/management/commands/update_lists.py
@@ -33,12 +33,12 @@ class Command(BaseCommand):
         for cl in conseils:
             if(cl.adhesions.all().count() > 0):
                 for e in cl.adhesions.exclude(role=membre):
-                    if cl.primaire and lists['responsables-clp'] not in e.listes.all() and not e.optout:
-                        print 'primaire',cl,e
-                        #lists['responsables-clp'].subscribe(e.email, to_ascii(e.prenom), to_ascii(e.nom))
-                        #e.listes.add(lists['responsables-clp'])
-                    if cl.secondaire and lists['responsables-cls'] not in e.listes.all() and not e.optout:
-                        print 'secondaire',cl,e
-                        #lists['responsables-cls'].subscribe(e.email, to_ascii(e.prenom), to_ascii(e.nom))
-                        #e.listes.add(lists['responsables-cls'])
+                    if cl.primaire and lists['responsables-clp'] not in e.adherent.listes.all() and not e.adherent.optout:
+                        print 'primaire',cl,e.adherent
+                        #lists['responsables-clp'].subscribe(e.adherent.email, to_ascii(e.adherent.prenom), to_ascii(e.adherent.nom))
+                        #e.adherent.listes.add(lists['responsables-clp'])
+                    if cl.secondaire and lists['responsables-cls'] not in e.adherent.listes.all() and not e.adherent.optout:
+                        print 'secondaire',cl,e.adherent
+                        #lists['responsables-cls'].subscribe(e.adherent.email, to_ascii(e.adherent.prenom), to_ascii(e.adherent.nom))
+                        #e.adherent.listes.add(lists['responsables-cls'])
         
